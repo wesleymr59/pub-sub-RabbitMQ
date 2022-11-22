8cc1d6c99d205a318a0b78a6a0d4804396b1d772
Wesley Marques <wesley.marques@ftdata.com.br>
2022-11-22 17:18:41 -0300

    Feat: adição da função 'Debug' para debuggar variaveis

--- services/functions.py
+++ services/functions.py
@@ -1,5 +1,35 @@
-import pika, json
+import pika, json, inspect, tabulate, sys
 
+
+def debug(*args,exitDebug):
+
+    # Busca o contexto de quem chamou a função debug:
+    context = inspect.stack()[1][0]
+
+    # Obtém detalhes de onde foi executado o debug:
+    filename = context.f_code.co_filename
+    linenumber = context.f_lineno
+
+    # Resultados a serem exibidos:
+    result = []
+
+    # Percorre todas as variáveis a serem exibidas:
+    for name in args:
+        # Verifica se é uma variável local no contexto:
+        if name in context.f_locals:
+            result.append([name, context.f_locals[name], type(context.f_locals[name])])
+        # Verifica se é uma variável global no contexto:
+        elif name in context.f_globals:
+            result.append([name, context.f_globals[name],type(context.f_locals[name])])
+        # Variável não encontrada no contexto:
+        else:
+            result.append([name, "Variable Not Found"])
+
+    # Exibe os resultados em forma de tabela:
+    print(f'[DEBUG] {filename} ({linenumber})')
+    print(tabulate.tabulate(result, headers=['Variável', 'Valor', 'Type' ]))
+    if exitDebug:
+            sys.exit("Leaving The Debugger Running")
 class QueueRabbit():
     def __init__(self) -> None:
         parameters = pika.ConnectionParameters(host="192.168.1.109",port=5672,virtual_host='/', 
